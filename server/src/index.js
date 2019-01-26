00729172e37cf0928f778ea58827dacbcba77393
Marc Nitzsche <marc.nitzsche@live.de>
2019-01-27 00:06:32 +0100

    Use esm for imports and exports

--- server/src/index.js
+++ server/src/index.js
@@ -1,29 +1,4 @@
-import express from 'express';
-import { ApolloServer, gql } from 'apollo-server-express';
-
-// Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
-
-// Provide resolver functions for your schema fields
-const resolvers = {
-  Query: {
-    hello: () => 'Hello world!'
-  }
-};
-
-const server = new ApolloServer({ typeDefs, resolvers });
-
-const app = express();
-server.applyMiddleware({ app });
-
-const port = 4000;
-
-app.listen({ port }, () =>
-  console.log(
-    `🚀 Server ready at http://localhost:${port}${server.graphqlPath}`
-  )
-);
+/* eslint-disable-next-line no-global-assign */
+require = require('esm')(module);
+require('module-alias/register');
+module.exports = require('./main.js');
