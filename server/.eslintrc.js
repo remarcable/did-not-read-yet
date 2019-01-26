f5f789c244c7861555a489a17e02baa6f832d7e2
Marc Nitzsche <marc.nitzsche@live.de>
2019-01-27 00:29:09 +0100

    Improve dev experience for jest

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,15 +1,21 @@
 module.exports = {
-  extends: ['eslint:recommended', 'airbnb-base', 'prettier'],
-  plugins: ['import', 'prettier'],
-  rules: {
-    'prettier/prettier': 'warn',
-  },
-  settings: {
-    'import/resolver': {
-      alias: {
-        map: [['@root', './src']],
-        extensions: ['.js', '.jsx', '.json'],
-      },
+    extends: ['eslint:recommended', 'airbnb-base', 'prettier'],
+    env: {
+        node: true,
+        es6: true,
+        jest: true,
+        jasmine: true,
     },
-  },
+    plugins: ['import', 'prettier'],
+    rules: {
+        'prettier/prettier': 'warn',
+    },
+    settings: {
+        'import/resolver': {
+            alias: {
+                map: [['@root', './src']],
+                extensions: ['.js', '.jsx', '.json'],
+            },
+        },
+    },
 };
