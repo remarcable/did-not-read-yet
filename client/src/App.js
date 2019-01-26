77cbe4b7619957f5d871da8ee479e669acde2e2e
Marc Nitzsche <marc.nitzsche@live.de>
2019-01-27 00:39:38 +0100

    Change tabwidth for prettier in client code

--- client/src/App.js
+++ client/src/App.js
@@ -4,76 +4,76 @@ import { modifyElement } from './lib/modifyElement';
 import DisplayLink from './components/DisplayLink';
 
 class App extends PureComponent {
-  state = {
-    links: [
-      {
-        id: 1,
-        title: 'Google',
-        url: 'https://google.com',
-        dismissed: false,
-        read: false,
-      },
-      {
-        id: 2,
-        title: 'YouTube',
-        url: 'https://youtube.com',
-        dismissed: false,
-        read: false,
-      },
-      {
-        id: 3,
-        title: 'Hacker News',
-        url: 'https://news.ycombinator.com',
-        dismissed: false,
-        read: false,
-      },
-    ],
-  };
+    state = {
+        links: [
+            {
+                id: 1,
+                title: 'Google',
+                url: 'https://google.com',
+                dismissed: false,
+                read: false,
+            },
+            {
+                id: 2,
+                title: 'YouTube',
+                url: 'https://youtube.com',
+                dismissed: false,
+                read: false,
+            },
+            {
+                id: 3,
+                title: 'Hacker News',
+                url: 'https://news.ycombinator.com',
+                dismissed: false,
+                read: false,
+            },
+        ],
+    };
 
-  updateSingleLink = (id, updatedFields) => {
-    this.setState(({ links }) => {
-      return {
-        links: modifyElement({ elements: links, id, updatedFields }),
-      };
-    });
-  };
+    updateSingleLink = (id, updatedFields) => {
+        this.setState(({ links }) => {
+            return {
+                links: modifyElement({ elements: links, id, updatedFields }),
+            };
+        });
+    };
 
-  markAsRead = id => {
-    this.updateSingleLink(id, { read: true });
-  };
+    markAsRead = id => {
+        this.updateSingleLink(id, { read: true });
+    };
 
-  dismissLink = id => {
-    this.updateSingleLink(id, { dismissed: true });
-  };
+    dismissLink = id => {
+        this.updateSingleLink(id, { dismissed: true });
+    };
 
-  undismissLink = id => {
-    this.updateSingleLink(id, { dismissed: false });
-  };
+    undismissLink = id => {
+        this.updateSingleLink(id, { dismissed: false });
+    };
 
-  render() {
-    const { links } = this.state;
-    const displayLink = link => (
-      <DisplayLink
-        key={link.id}
-        link={link}
-        markAsRead={this.markAsRead}
-        dismissLink={this.dismissLink}
-        undismissLink={this.undismissLink}
-      />
-    );
+    render() {
+        const { links } = this.state;
+        const displayLink = link => (
+            <DisplayLink
+                key={link.id}
+                link={link}
+                markAsRead={this.markAsRead}
+                dismissLink={this.dismissLink}
+                undismissLink={this.undismissLink}
+            />
+        );
 
-    return (
-      <div>
-        <h1>Did not read yet</h1>
-        <h2>New Links</h2>
-        {links.filter(link => !link.read && !link.dismissed).map(displayLink)}
-        <h2>Read Links</h2>
-        {links.filter(link => link.read && !link.dismissed).map(displayLink)}
-        <h2>Dismissed Links</h2>
-        {links.filter(link => link.dismissed).map(displayLink)}
-      </div>
-    );
-  }
+        return (
+            <div>
+                <h1>Did not read yet</h1>
+                <h2>New Links</h2>
+                {links.filter(link => !link.read && !link.dismissed).map(displayLink)}
+                <h2>Read Links</h2>
+                {links.filter(link => link.read && !link.dismissed).map(displayLink)}
+                <h2>Dismissed Links</h2>
+                {links.filter(link => link.dismissed).map(displayLink)}
+            </div>
+        );
+    }
 }
 
 export default App;
--- client/src/components/DisplayLink/index.js
+++ client/src/components/DisplayLink/index.js
@@ -1,25 +1,20 @@
 import React from 'react';
 
 const DisplayLink = ({ link, markAsRead, dismissLink, undismissLink }) => {
-  const { id, title, url, dismissed } = link;
+    const { id, title, url, dismissed } = link;
 
-  return (
-    <div>
-      <a
-        href={url}
-        target="_blank"
-        rel="noreferrer noopener"
-        onClick={() => markAsRead(id)}
-      >
-        {title}
-      </a>
-      {dismissed ? (
-        <button onClick={() => undismissLink(id)}>Undismiss</button>
-      ) : (
-        <button onClick={() => dismissLink(id)}>Dismiss</button>
-      )}
-    </div>
-  );
+    return (
+        <div>
+            <a href={url} target="_blank" rel="noreferrer noopener" onClick={() => markAsRead(id)}>
+                {title}
+            </a>
+            {dismissed ? (
+                <button onClick={() => undismissLink(id)}>Undismiss</button>
+            ) : (
+                <button onClick={() => dismissLink(id)}>Dismiss</button>
+            )}
+        </div>
+    );
 };
 
 export default DisplayLink;
